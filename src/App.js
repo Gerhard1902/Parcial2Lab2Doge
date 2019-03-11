49cae21e62a38b5dfa4aee7791a073117232b6f3
Gerhard1902 <32085719+Gerhard1902@users.noreply.github.com>
2019-03-10 21:18:34 -0600

    Paso 2 de 4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import doge from "./doge.jpg";
 import './App.css';
 
-class App extends Component {
+class DogeMeme extends Component {
+  constructor(props){
+  	super(props);
+  	this.state={
+  		message1: this.props.top,
+  		message2: this.props.bottom
+  	};
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    	
+      <div className="Doge">
+       	<p>{this.state.message1}</p>
+       	<p className="lol">{this.state.message2}</p>
       </div>
     );
   }
 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+export default DogeMeme;
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
