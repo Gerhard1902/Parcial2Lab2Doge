c7167e1dadc59657222b68b36463e99cedc18bb7
Gerhard1902 <32085719+Gerhard1902@users.noreply.github.com>
2019-03-10 22:37:51 -0600

    3 de 4 Changed my mind, terminado

--- src/App.js
+++ src/App.js
@@ -5,26 +5,42 @@ import './App.css';
 
 class DogeMeme extends Component {
   constructor(props){
-  	super(props);
-  	this.state={
-  		message1: this.props.top.toUpperCase(),
-  		message2: this.props.bottom.toUpperCase()
-  	};
+    super(props);
+    this.state={
+      message1: "",
+      message2: "",
+    };
+     // This binding is necessary to make `this` work in the callback
+     //https://reactjs.org/docs/handling-events.html
+    this.handleFirst = this.handleFirst.bind(this);
+    this.handleSecond = this.handleSecond.bind(this);
+  }
+  handleFirst(event) {
+    this.setState({message1: event.target.value.toUpperCase()});
+  }
+  handleSecond(event) {
+    this.setState({message2: event.target.value.toUpperCase()});
   }
   render() {
     return (
-    	
-      <div className="Doge">
-       	<p>{this.state.message1}</p>
-       	<p>{this.state.message2}</p>
+       <div>
+        <form>
+          <input type="text"  onChange={this.handleFirst} />
+          <input type="text"  onChange={this.handleSecond} />
+        </form>
+        <div className="Doge">
+          <p>{this.state.message1}</p>
+          <p>{this.state.message2}</p>
+        </div>
       </div>
+      
     );
   }
 }
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 export default DogeMeme;
 
-
+ 
 
 
 
